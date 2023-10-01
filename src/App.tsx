a4ebd86056c8c359c3a6348c54c4d34594d6651d
Ola Haldor <olahh@ntnu.no>
2023-10-01 17:00:03 +0200

    feat: make footer
    add footer and add in app
    
    issue: 5

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
+// App.jsx
 import React from 'react';
 import Header from "./components/molecules/Header";
-import './App.css'
+import Footer from "./components/molecules/Footer";
 
-function App() {
-    return (
-        <>
-        <div>
-            <Header />
-        </div>
-            
-        </>
-        
-    );
-}
+const App: React.FC = () => {
+  return (
+    <div className="flex flex-col h-screen justify-between">
+      <div>
+        <Header />
+        <Footer />
+      </div>
+    </div>
+  );
+};
 
-export default App
+export default App;
