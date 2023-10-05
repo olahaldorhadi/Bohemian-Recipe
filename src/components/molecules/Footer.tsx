163ba4bd378529dfd1c80145c29414169990f66c
ottohf <otto.fearnley@gmail.com>
2023-10-05 13:28:20 +0200

    style: run prettier and fix project formatting
    Ran prettier on the whole project, fixing the formatting.
    
    Issue: #11

--- src/components/molecules/Footer.tsx
+++ src/components/molecules/Footer.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
+import React from 'react'
 
 const Footer: React.FC = () => {
-  return (
-    <footer className="bottom-0 left-0 w-full bg-black h-10 flex items-center justify-center">
-      <p className="text-orange-300 text-xs">
-        &copy; {new Date().getFullYear()} Bohemian Recipe
-      </p>
-    </footer>
-  );
-};
+    return (
+        <footer className="bottom-0 left-0 w-full bg-black h-10 flex items-center justify-center">
+            <p className="text-orange-300 text-xs">
+                &copy; {new Date().getFullYear()} Bohemian Recipe
+            </p>
+        </footer>
+    )
+}
 
-export default Footer;
+export default Footer
--- src/components/molecules/RecipeCard.tsx
+++ src/components/molecules/RecipeCard.tsx
@@ -7,12 +7,21 @@ type RecipeCardProps = {
     category: string
 }
 
-const RecipeCard: React.FC<RecipeCardProps> = ({ imgSrc, imgAlt, title, category }) => {
+const RecipeCard: React.FC<RecipeCardProps> = ({
+    imgSrc,
+    imgAlt,
+    title,
+    category,
+}) => {
     return (
-        <div className='w-1/5 h-96 rounded-md border-2 border-slate-200 bg-zinc-900'>
-            <img src={imgSrc} alt={imgAlt} className='rounded-t-md w-full h-1/2 object-cover' />  
-            <h2 className='text-2xl m-2'>{title}</h2>
-            <p className='mx-2'>{category}</p>
+        <div className="w-1/5 h-96 rounded-md border-2 border-slate-200 bg-zinc-900">
+            <img
+                src={imgSrc}
+                alt={imgAlt}
+                className="rounded-t-md w-full h-1/2 object-cover"
+            />
+            <h2 className="text-2xl m-2">{title}</h2>
+            <p className="mx-2">{category}</p>
         </div>
     )
 }
