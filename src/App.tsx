4e291533847fc1118973222f97fb4ba474dfc45f
Ola Haldor <olahh@ntnu.no>
2023-10-10 14:47:29 +0200

    feature: add modal to display recipe and its instructions
    added a modal to display more information about the recipe. Possibility to close with button and just on click outside of modal
    
    Co-authored-by: Hanna Lunne hannalun@ntnu.no
    
    Issue: #12

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,44 @@
-// App.jsx
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './components/molecules/Header'
 import DinnerPicture from './components/molecules/FeatureSection'
 import Footer from './components/molecules/Footer'
 import RecipeCard from './components/molecules/RecipeCard'
+import ModalRecipe from './components/molecules/Modal/ModalRecipe'
 
 const App: React.FC = () => {
+    const [modalVisible, setModalVisible] = useState(false)
+
+    const handleRecipeCardClick = () => {
+        setModalVisible(true)
+    }
+
+    const handleCloseModal = () => {
+        setModalVisible(false)
+    }
+
+    const recipeDetails = {
+        imgSrc: 'https://www.themealdb.com/images//media/meals/ypxvwv1505333929.jpg',
+        imgAlt: 'Crock Pot Chicken Baked Tacos',
+        title: 'Crock Pot Chicken Baked Tacos',
+        category: 'Chicken',
+        steps: 'Cook the chicken with spices until tender.',
+    }
+
     return (
         <div className="flex flex-col h-screen justify-between">
             <div>
                 <Header />
                 <DinnerPicture />
                 <RecipeCard
-                    imgSrc={
-                        'https://www.themealdb.com/images//media/meals/ypxvwv1505333929.jpg'
-                    }
-                    imgAlt={'Crock Pot Chicken Baked Tacos'}
-                    title={'Crock Pot Chicken Baked Tacos'}
-                    category={'Chicken'}
+                    onClick={handleRecipeCardClick}
+                    {...recipeDetails}
                 />
+                {modalVisible && (
+                    <ModalRecipe
+                        recipe={recipeDetails}
+                        onClose={handleCloseModal}
+                    />
+                )}
                 <Footer />
             </div>
         </div>
