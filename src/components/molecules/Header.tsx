f42ea3647c73bd5f5b59938964524af5df7a3ada
Ola Haldor Hådi <olahh@stud.ntnu.no>
2023-10-10 16:37:46 +0200

    enhancement: improve styling on header

--- src/components/molecules/Header.tsx
+++ src/components/molecules/Header.tsx
@@ -3,13 +3,14 @@ import Logo from '../../assets/BR-logo-tr.png'
 
 const Header: React.FC = () => {
     return (
-        <div className="w-full bg-black flex items-center justify-center py-1">
-            <div className="h-10">
+        <div className="w-full bg-black flex items-center justify-center py-2 md:py-2">
+            <div className="h-16 md:h-28">
                 <a href="http://www.vg.no" className="header-logo-link">
-                    <img className="h-10" src={Logo} alt="Logo" />
+                    <img className="h-full" src={Logo} alt="Logo" />
                 </a>
             </div>
         </div>
+
     )
 }
 
