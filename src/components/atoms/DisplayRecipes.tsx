6ad96b403bd7703371f1030a45823d34d992edea
Hanna Lunne <hannalun@stud.ntnu.no>
2023-11-08 13:59:39 +0100

    feat: implement pagination with queries using offset

--- src/components/atoms/DisplayRecipes.tsx
+++ src/components/atoms/DisplayRecipes.tsx
@@ -6,8 +6,8 @@ import { gql, useQuery } from '@apollo/client'
 // Query accepts two optional parameters (categories and areas, both are strings)
 // Query should return idMeal, strMeal, strCategory and strArea. Can be expanded to include all necessary information.
 const GET_MEALS = gql`
-    query GetMeals($categories: [String], $areas: [String]) {
-        meals(categories: $categories, areas: $areas) {
+    query GetMeals($categories: [String], $areas: [String], $offset: Int!) {
+        meals(categories: $categories, areas: $areas, offset: $offset) {
             idMeal
             strMeal
             strCategory
@@ -72,11 +72,23 @@ interface DisplayRecipesProps {
     selectedAreas: string[]
 }
 
+const PAGE_SIZE = 12;
+
 const DisplayRecipes: React.FC<DisplayRecipesProps> = ({
     selectedCategory,
     selectedAreas,
 }) => {
     const [selectedRecipe, setSelectedRecipe] = useState<Meal | null>(null)
+    const [currentPage, setCurrentPage] = useState(0);
+
+    const { loading, error, data, fetchMore } = useQuery(GET_MEALS, {
+        variables: {
+            categories: selectedCategory ? [selectedCategory] : [],
+            areas: selectedAreas,
+            offset: currentPage,
+        },
+        notifyOnNetworkStatusChange: true, // To update 'loading' on refetching
+    });
 
     const handleRecipeCardClick = (meal: Meal) => {
         setSelectedRecipe(meal)
@@ -86,10 +98,27 @@ const DisplayRecipes: React.FC<DisplayRecipesProps> = ({
         setSelectedRecipe(null)
     }
 
-    // Uses the query GET_MEALS with variables defined under
-    const { loading, error, data } = useQuery(GET_MEALS, {
-        variables: { categories: selectedCategory, areas: selectedAreas },
-    })
+    const pageBack = () => {
+        if (currentPage > 0) {
+            setCurrentPage(currentPage - 1);
+            fetchMore({
+                variables: {
+                    offset: (currentPage - 1) * PAGE_SIZE,
+                },
+            });
+        }
+    };
+
+    const pageForward = () => {
+        if (data && data.meals.length === PAGE_SIZE) {
+            setCurrentPage(currentPage + 1);
+            fetchMore({
+                variables: {
+                    offset: (currentPage + 1) * PAGE_SIZE,
+                },
+            });
+        }
+    };
 
     if (loading) return <p>Loading meals...</p>
     if (error) return <p>Error loading meals: {error.message}</p>
@@ -112,6 +141,11 @@ const DisplayRecipes: React.FC<DisplayRecipesProps> = ({
                         />
                     </div>
                 ))}
+            <div className='w-full flex justify-center items-center'>
+                <button disabled={currentPage === 0} onClick={pageBack}>⬅</button>
+                <output className='mx-6'>{currentPage + 1}</output>
+                <button disabled={data && data.meals.length < PAGE_SIZE} onClick={pageForward}>➡</button>
+            </div>
             {selectedRecipe && (
                 <ModalRecipe
                     selectedRecipe={selectedRecipe}
